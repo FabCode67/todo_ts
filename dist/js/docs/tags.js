39ff1838c74a38616f2f7d179f27371d52b67367
Fabrice <mwanafunzifabrice@gmail.com>
2023-06-05 19:47:34 +0200

    sixth commit

--- dist/js/docs/tags.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    tags: [
-        {
-            name: "Todo CRUD operations", // name of a tag
-        },
-    ],
-};
